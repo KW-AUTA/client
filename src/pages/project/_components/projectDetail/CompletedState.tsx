36b8fa8023b20051b74f326f8df17a5f9c38f950
youjin-hong <asdfghjk1052@naver.com>
2025-07-10 23:18:49 +0900

    [ #91 | Refactor ] 데이터 흘러넘칠 때 scroll 처리를 위한 부모 컴포넌트 높이 부여

--- src/pages/project/_components/projectDetail/CompletedState.tsx
+++ src/pages/project/_components/projectDetail/CompletedState.tsx
@@ -7,7 +7,7 @@ interface CompleteStateProps {
 }
 export default function CompletedState({ projectDetail }: CompleteStateProps) {
   return (
-    <section className="flex gap-8 mt-4 mb-6 children:shadow-custom children:rounded-15 children:bg-transparent">
+    <section className="flex gap-8 mt-4 mb-6 h-72 children:shadow-custom children:rounded-15 children:bg-transparent">
       {projectDetail.testSummary && <ProjectSummaryGraph testSummary={projectDetail.testSummary} />}
       <ProjectPageTable pages={projectDetail?.pages || []} />
     </section>

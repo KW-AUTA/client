7af41e73a9814621ecec1ba7484c6832d9b8a6d2
youjin-hong <asdfghjk1052@naver.com>
2025-07-26 17:47:44 +0900

    [ #99 | Refactor ] projectStatus별 섹션 컴포넌트 생성

---
+++ src/pages/project/_components/projectDetail/CompletedStateSection.tsx
@@ -0,0 +1,15 @@
+import LLMResult from '@/pages/project/_components/projectDetail/LLMResult';
+import TestResult from '@/pages/project/_components/projectDetail/TestResult';
+import { ProjectDetailData } from '@/types/project.type';
+
+interface CompleteStateProps {
+  projectDetail: ProjectDetailData;
+}
+export default function CompletedStateSection({ projectDetail }: CompleteStateProps) {
+  return (
+    <section>
+      <LLMResult />
+      <TestResult projectDetail={projectDetail} />
+    </section>
+  );
+}

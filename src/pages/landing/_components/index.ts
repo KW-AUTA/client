bc61c16b7089ee7950abccb7242db4e6ef5bd980
hywznn <choihj0510@naver.com>
2025-07-30 11:16:46 +0900

    [ #96 | feat ] 컴포넌트 index 및 css 커스텀

---
+++ src/pages/landing/_components/index.ts
@@ -0,0 +1,8 @@
+export { default as Navigation } from './Navigation';
+export { default as HeroSection } from './HeroSection';
+export { default as HowItWorksSection } from './HowItWorksSection';
+export { default as AboutSection } from './AboutSection';
+export { default as FinalCTASection } from './FinalCTASection';
+export { default as Footer } from './Footer';
+export { default as AnimatedCard } from './AnimatedCard';
+export { default as FeatureBadge } from './FeatureBadge';
